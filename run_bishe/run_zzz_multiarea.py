0e7c3cc997e0cd825bd87fde6d0a2afb555c306c
Eric <ericzs2017@outlook.com>
2025-04-22 11:02:50 +0800

    在run_zzz_multiarea.py中添加打印hparams.clustering的语句以便调试

--- run_bishe/run_zzz_multiarea.py
+++ run_bishe/run_zzz_multiarea.py
@@ -157,6 +157,7 @@ if __name__ == "__main__":
     else:
         print("训练路由器...")
         router = KnowRouter(cfg=hparams)
+        print(hparams.clustering)
         router.build_route_table(prompt_list=prompts)
         if args.router_save_path:
             try:
