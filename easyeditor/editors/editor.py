730e057fa9b5d5bd222cae6cb17bdda94f9a726e
Eric <ericzs2017@outlook.com>
2025-05-11 09:11:04 +0800

    测试实验一

--- easyeditor/editors/editor.py
+++ easyeditor/editors/editor.py
@@ -400,8 +400,8 @@ class BaseEditor:
                     all_metrics[idx]['pre'].pop('locality')
 
             if verbose:
-                pass
                 # LOG.info(f"{idx} editing: {request['prompt']} -> {request['target_new']}  \n\n {all_metrics[idx]}")
+                LOG.info(f"{idx} editing: {all_metrics[idx]}")
 
         if sequential_edit:
             for i, request in enumerate(tqdm(requests, total=len(requests))):
